11e90b3bc8e225694c8bc5806af187966fd6ca07
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-01 15:05:55 +0000

    Employee

--- kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/Employee.java
+++ kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/Employee.java
@@ -2,6 +2,7 @@ package com.example.kotlinCRUD;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 public class Employee {
     private @Id @GeneratedValue Long id;
@@ -39,5 +40,24 @@ public class Employee {
     }
 
     @Override
-    public boolean equ
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+        if (!(o instanceof Employee))
+            return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
+                && Objects.equals(this.role, employee.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+    }
 }
