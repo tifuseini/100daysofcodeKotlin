4ba1c87e4d808c4f9ccc73a2a1f093043dd1534a
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-08 21:46:28 +0000

    Test Application using Spring Boot- KotlinCrud

--- kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/Employee.kt
+++ kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/Employee.kt
@@ -5,7 +5,8 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
-class Employee(@Id @GeneratedValue val id:Long,
-               val firstName:String,
-               val lastName:String,
-               val age:Int)
+data class Employee(
+    @Id @GeneratedValue val id:Long,
+    val firstName:String,
+    val lastName:String,
+    val age:Int)
