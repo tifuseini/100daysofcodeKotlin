c2619c05783e6c59f4f90d931ef430883acd050a
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-11 22:06:18 +0000

    Added data to mobile support

--- mobile-Number/src/main/kotlin/com/example/mobile/number/MobileNumberApplication.kt
+++ mobile-Number/src/main/kotlin/com/example/mobile/number/MobileNumberApplication.kt
@@ -1,10 +1,18 @@
 package com.example.mobile.number
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class MobileNumberApplication
+class MobileNumberApplication{
+
+	private val log = LoggerFactory.getLogger(MobileNumberApplication::class.java)
+
+	@Bean
+	fun init(repo)
+}
 
 
 
