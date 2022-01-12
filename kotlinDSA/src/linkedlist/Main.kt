01e23ce0ffa03341ef3472622f64e11fcd547595
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-12 21:12:41 +0000

    Linkedlist-1

--- kotlinDSA/src/linkedlist/Main.kt
+++ kotlinDSA/src/linkedlist/Main.kt
@@ -1,14 +1,17 @@
 package linkedlist
 
 fun main() {
-    "creating and linking nodes" example{
-        val node1 = Node(value = 1)
-        val node2 = Node(value = 2)
-        val node3 = Node(value = 3)
+    println("Creating and linking nodes")
+    val node1 = Node(value = 1)
+    val node2 = Node(value = 2)
+    val node3 = Node(value = 3)
+    val node4 = Node(value = 4)
 
-        node1.next = node2
-        node2.next = node3
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
 
-        print(node1)
-    }
-}
+    println(node1)
+
+    println(node3)
+ }
--- kotlinDSA/src/linkedlist/Node.kt
+++ kotlinDSA/src/linkedlist/Node.kt
@@ -1,10 +1,10 @@
 package linkedlist
 
-data class Node<T>(var value: T,var next: Node<T>? = null) {
+data class Node<T>(var value: T,var next : Node<T>? = null){
     override fun toString(): String {
         return if (next != null){
             "$value -> ${next.toString()}"
-        } else{
+        } else {
             "$value"
         }
     }
