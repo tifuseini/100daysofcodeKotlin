263473455e3353365143e29685e0d5e7ef5acc36
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-16 21:28:47 +0000

    Random Dice Final

--- day013/src/Dice2.kt
+++ day013/src/Dice2.kt
@@ -1,15 +1,17 @@
-class Dice2 {
-    var sides = 6
+class Dice2(val numSides : Int) {
 
-    fun roll(){
-        val randomNumber = (1..6).random()
-        println(randomNumber)
+    fun roll() : Int{
+       return (1..numSides).random()
     }
 }
 
 fun main() {
-    val myFirstDice = Dice2()
+    val myFirstDice = Dice2(6)
+    println("Your ${myFirstDice.numSides} sided " +
+            "dice rolled ${myFirstDice.roll()}")
 
-    println(myFirstDice.sides)
-    myFirstDice.roll()
+   val mySecondDice = Dice2(27)
+    println("Your ${mySecondDice.numSides} sided " +
+            "dice rolled ${mySecondDice.roll()}")
+
 }
