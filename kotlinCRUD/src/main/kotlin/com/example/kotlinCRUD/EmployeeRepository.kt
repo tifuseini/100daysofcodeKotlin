b553245db7d077706517c1425d086bf6f7763e43
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-20 22:21:05 +0000

    Kotlin Crud Update

--- kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/EmployeeRepository.kt
+++ kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/EmployeeRepository.kt
@@ -1,11 +1,11 @@
 package com.example.kotlinCRUD
 
-import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 
 
 interface EmployeeRepository : CrudRepository<Employee,String>{
 
     @Query("select * from messages")
-    fun findMessages(): List<Employee>
+    fun findEmployees(): List<Employee>
 }
