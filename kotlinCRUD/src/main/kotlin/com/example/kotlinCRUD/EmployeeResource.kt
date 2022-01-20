1b0bfe16b8ffbc5de0099793ac60c961034a3a52
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-20 22:21:37 +0000

    Kotlin Crud Update

--- kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/EmployeeResource.kt
+++ kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/EmployeeResource.kt
@@ -1,4 +1,18 @@
 package com.example.kotlinCRUD
 
-class EmployeeResource {
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EmployeeResource(val service: EmployeeService) {
+
+    @GetMapping
+    fun index(): List<Employee> = service.findEmployees()
+
+    @PostMapping
+    fun post(@RequestBody employee: Employee) {
+        service.post(employee)
+    }
 }
