1a27d3df2a3b67374a0bae3bf0728478099f248e
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-20 22:22:10 +0000

    Kotlin Crud Update

--- kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/EmployeeService.kt
+++ kotlinCRUD/src/main/kotlin/com/example/kotlinCRUD/EmployeeService.kt
@@ -1,2 +1,13 @@
 package com.example.kotlinCRUD
 
+import org.springframework.stereotype.Service
+
+@Service
+class EmployeeService (val db: EmployeeRepository){
+
+    fun findEmployees() : List<Employee> = db.findEmployees()
+
+    fun post(employee: Employee){
+        db.save(employee)
+    }
+}
