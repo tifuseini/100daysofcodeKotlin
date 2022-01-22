910304c6a8f1bbecb2fad2ba1c0b1f0dd6319770
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-22 22:29:46 +0000

    Loop

---
+++ day017/src/Loop.kt
@@ -0,0 +1,18 @@
+import kotlin.random.Random
+
+fun main() {
+    var sum = 10
+
+    while (sum < 1000){
+        sum += sum
+        print(sum)
+    }
+
+    var counter = 0
+
+    while (counter < 10){
+        counter++
+        print(counter)
+    }
+
+}
