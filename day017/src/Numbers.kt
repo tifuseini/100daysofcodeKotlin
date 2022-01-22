e5cdb5d977f7c01e8c5c0e91e79173fca3d500a7
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-22 21:33:06 +0000

    Pair Triple 2

---
+++ day017/src/Numbers.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val a: Short = 12
+    val b: Byte = 120
+    val c: Int = 1000000
+
+    val answer = a + b + c
+    println(answer)
+
+    val anyNumber:Any = 45
+    val anyString: Any = "45"
+
+    println(anyString)
+    println(anyNumber)
+
+
+}
+
+
