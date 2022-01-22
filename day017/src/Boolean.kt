fdc4f29b03dea6ca74ad566493ec798e458e3be6
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-22 21:48:58 +0000

    Boolean

---
+++ day017/src/Boolean.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    val yes:Boolean = true
+    val no:Boolean = false
+
+    val yes1 = true
+    val no1 = false
+
+    val doesOneEqualTwo = (1 == 2)
+    println(doesOneEqualTwo)
+
+    val doesOneNotEqualTwo = (1 != 2)
+    println(doesOneNotEqualTwo)
+
+    val alsoTrue= !(1 == 2)
+    println(alsoTrue)
+
+    val and = true && true
+    println(and)
+
+    val or = true || false
+    println(or)
+}
