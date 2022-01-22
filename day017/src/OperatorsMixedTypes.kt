95dec6e2e6ab5d3ea0945ae171328a5704403231
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-22 19:12:21 +0000

    Operators with mixed types

---
+++ day017/src/OperatorsMixedTypes.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val hourlyRate:Double = 10.4
+    val hoursWorked:Int = 10
+    val totalCost: Double = hourlyRate * hoursWorked
+
+    println(totalCost)
+}
+
+class OperatorsMixedTypes{
+
+    val typeInferredInt = 0.5
+}
