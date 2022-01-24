1d6157ad1045cf41ccf87409ab41e42b79389731
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-24 22:11:35 +0000

    Function

---
+++ day019/src/chapter6/printMyName.kt
@@ -0,0 +1,20 @@
+package chapter6
+
+fun printMyName(){
+    println("My name is Tahiru")
+}
+
+fun printMultiplesOfFive(value:Int){
+    println("$value * 5 = ${value * 5}")
+}
+
+fun printMultiplesOf(multiplier:Int,andValue:Int){
+    println("$multiplier * $andValue = ${multiplier * andValue}")
+}
+
+fun main() {
+
+    printMyName()
+    printMultiplesOfFive(90)
+    printMultiplesOf(2,6)
+}
