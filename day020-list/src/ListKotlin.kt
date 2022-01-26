4dcd31281cdb645d2cbcbc14611b52f9d4c0c6e9
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-26 22:35:18 +0000

    new

---
+++ day020-list/src/ListKotlin.kt
@@ -0,0 +1,20 @@
+open class Item(val name: String, val price: Int)
+
+class Noodles : Item("Noodles", 10){
+    override fun toString(): String {
+        return name
+    }
+}
+
+class Vegetables : Item("Vegetables", 5){
+    override fun toString(): String {
+        return name
+    }
+}
+
+fun main() {
+    val noodles = Noodles()
+    val vegetables = Vegetables("Cabbages","Sprouts")
+    println(noodles)
+    println(vegetables)
+}
