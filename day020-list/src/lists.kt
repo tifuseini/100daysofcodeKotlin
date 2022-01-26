13207baf84c478147cb83dc883687c0a5c101399
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-26 21:55:19 +0000

    List

---
+++ day020-list/src/lists.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    val number:List<Int> = listOf(1,2,3,4,5,6)
+
+    val number1 = listOf(1,2,3,4,9,5,6)
+
+
+
+    println(number)
+    println(number1)
+    println(number.last())
+    println()
+}
