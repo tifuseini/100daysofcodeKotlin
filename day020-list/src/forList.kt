89eae9a2d6b4896b4464a1063f0b091804091e3b
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-26 22:20:27 +0000

    For

---
+++ day020-list/src/forList.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val names = listOf<String>("Jessica","Henry","Alicia","Jose")
+
+    for (name in names){
+        println("$name - Number of characters: ${name.length}")
+    }
+}
