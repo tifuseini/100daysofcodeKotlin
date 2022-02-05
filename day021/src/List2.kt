f9ba4d665499a0bc3da89627f1919efd9c417a76
tifuseini <tifuseini@st.ug.edu.gh>
2022-02-05 13:46:00 +0000

    List 3rd Commit

--- day021/src/List2.kt
+++ day021/src/List2.kt
@@ -1,3 +1,11 @@
 fun main() {
-    v
+    val colors = listOf<String>("red","blue","green")
+    val colors2 = listOf("yellow","white","indigo")
+    val colors3: List<String> = listOf("aqua","sea-blue","orange")
+
+    println("Reversed list : ${colors.reversed()}")
+    println("List : ${colors}")
+    println("Sorted list : ${colors.sorted()}")
+
 }
+
