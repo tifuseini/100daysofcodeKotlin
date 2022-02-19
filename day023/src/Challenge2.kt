fd18fb49cd50381a62810c61668c26fb3f807989
tifuseini <tifuseini@st.ug.edu.gh>
2022-02-19 22:43:42 +0000

    Commit

--- day023/src/Challenge2.kt
+++ day023/src/Challenge2.kt
@@ -3,5 +3,13 @@ fun fibonacci(number:Int):Int{
         return 0
     }
 
-    if
+    if(number == 1 || number == 2){
+        return  1
+    }
+
+    return fibonacci(number - 1) + fibonacci(number -2)
+}
+
+fun main() {
+
 }
