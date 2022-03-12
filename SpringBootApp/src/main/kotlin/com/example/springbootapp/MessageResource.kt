5415f03bb55531c1058e24dec46d9b296347a1a5
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-12 20:54:02 +0000

    Message Resource

--- SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageResource.kt
+++ SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageResource.kt
@@ -1,4 +1,14 @@
 package com.example.springbootapp
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class MessageResource {
+    @GetMapping
+    fun index() : List<Message> = listOf(
+        Message("1","This is number 1"),
+        Message("2","This is number 2"),
+        Message("3","this is number 3")
+    )
 }
