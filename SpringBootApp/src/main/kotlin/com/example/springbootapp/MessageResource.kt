74a7ad4c6678547c585b5e40a2ee16dc3eaa55d1
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-12 22:41:08 +0000

    Message Resiure

--- SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageResource.kt
+++ SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageResource.kt
@@ -1,14 +1,17 @@
 package com.example.springbootapp
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MessageResource {
+class MessageResource(val service  : MessageService) {
     @GetMapping
-    fun index() : List<Message> = listOf(
-        Message("1","This is number 1"),
-        Message("2","This is number 2"),
-        Message("3","this is number 3")
-    )
+    fun index() : List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
 }
