ef80595ffa9888c9e8f3af07e60cb45f593f8ee6
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-12 21:09:21 +0000

    Messsage Repository

--- SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageRepository.kt
+++ SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageRepository.kt
@@ -1,4 +1,10 @@
 package com.example.springbootapp
 
-class MessageRepository {
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+
+interface MessageRepository : CrudRepository<Message,String> {
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
 }
