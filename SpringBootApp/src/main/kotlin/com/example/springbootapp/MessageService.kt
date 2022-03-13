b0e6c19ec429f82e593e1818258261c3242ec9da
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-13 16:19:23 +0000

    Message Service

--- SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageService.kt
+++ SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageService.kt
@@ -1,6 +1,8 @@
 package com.example.springbootapp
 
+import org.springframework.data.annotation.Id
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
 
 @Service
 class MessageService(val db:MessageRepository) {
@@ -10,4 +12,5 @@ class MessageService(val db:MessageRepository) {
     fun post(message: Message){
         db.save(message)
     }
+
 }
