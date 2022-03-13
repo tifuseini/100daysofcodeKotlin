af53505f52708e349d82dba4dbead46a746f54ba
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-13 16:29:49 +0000

    Starter

---
+++ SpringBootAppJPA/src/test/kotlin/com/example/springbootappjpa/SpringBootAppJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootappjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootAppJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
