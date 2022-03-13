b15cceb21577c4a6c0a76c5c2d97c619d1bc0f92
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-13 16:18:42 +0000

    Message Rsours

--- SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageResource.kt
+++ SpringBootApp/src/main/kotlin/com/example/springbootapp/MessageResource.kt
@@ -1,17 +1,21 @@
 package com.example.springbootapp
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class MessageResource(val service  : MessageService) {
+class MessageResource(
+    val service  : MessageService,
+    val repository: MessageRepository) {
     @GetMapping
     fun index() : List<Message> = service.findMessages()
 
     @PostMapping
     fun post(@RequestBody message: Message) {
         service.post(message)
+    }
+
+    @DeleteMapping("/id")
+    fun delete(@PathVariable id : String){
+        repository.deleteById(id)
     }
 }
