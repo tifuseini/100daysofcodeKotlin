e999a2d30347c18b15f63d871e3f87977b66e7ad
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-23 09:08:50 +0000

    A nullable Variavle

--- day024/src/BasicKotlin/nullable.kt
+++ day024/src/BasicKotlin/nullable.kt
@@ -1,9 +1,19 @@
 package BasicKotlin
 
+data class Person(
+    val first:String,
+    val middle:String?,
+    val last:String
+)
 
 fun main() {
 
-    val name : String = null
-    println(name)
+    val jkRowling = Person("Jonne",null,"ROwlings")
+    val northWest = Person("North",null,"West")
+    val tifuseini = Person("Tahiru","Iddrisu","Fuseini")
+
+    println(jkRowling)
+    println(northWest)
+    println(tifuseini)
 
 }
