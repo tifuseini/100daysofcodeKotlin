c5fcbeaf603ff1efe7a4203777f09dbec069f29c
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-25 22:43:04 +0000

    Defining compile Time constants

--- day024/src/BasicKotlin/Task.kt
+++ day024/src/BasicKotlin/Task.kt
@@ -1,4 +1,13 @@
 package BasicKotlin
 
-class Task {
+class Task(
+    val name: String,_priority:Int = DEFAULT_PRIORITY
+) {
+
+    companion object{
+        const val MIN_PRIORITY = 1
+        const val MAX_PRIORITY = 5
+        const val DEFAULT_PRIORITY = 3
+    }
+
 }
