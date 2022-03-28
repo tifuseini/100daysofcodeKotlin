56f6fc664794a247ca47a65a6f05eeb07d838afe
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-28 23:56:35 +0100

    Classes nd list

--- day025-Objects/src/User.kt
+++ day025-Objects/src/User.kt
@@ -1,1 +1,15 @@
-data class User()
+data class User(
+    val firstName:String,
+    val lastName: String
+)
+
+fun main() {
+    val brain = User("Tahiru","Fuseini")
+    val rose = User("Naziya","Sadiquw")
+
+    val attendees: MutableList<User> = mutableListOf(brain, rose)
+
+    attendees.forEach{
+        user -> println("$user is attending")
+    }
+}
