3e669b3beb55c1308842128e67da89897a7e1775
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-29 22:08:09 +0000

    Creating a nw property fot a Person

--- day025-Objects/src/Person.kt
+++ day025-Objects/src/Person.kt
@@ -1,11 +1,21 @@
 class Person (val firstName:String,val lastName:String) {
 
+    val fullname:String
+
+    init {
+        fullname = "$firstName $lastName"
+    }
+
+
     override fun toString(): String {
-        return "$firstName $lastName "
+        return fullname
     }
 }
 
 fun main() {
+
+    val tahiru = Person("Tahiru","Fuseini")
+    println(tahiru)
 
     val Brain = Person("Brain","Truesby")
     println(Brain.toString())
