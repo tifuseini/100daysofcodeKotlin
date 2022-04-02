a7372bdfb371fabc2db933eba4fc62b26c4c5303
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-02 20:50:35 +0000

    Updating Test

--- day025-Objects/src/main.kt
+++ day025-Objects/src/main.kt
@@ -19,19 +19,19 @@ fun main() {
     println(Iddrisu)
 
     if (Tahiru.equals(Iddrisu))
-        println("It's the same person")
+        println("Tahiru and Iddrisu are the same")
     else
-        println("Nope not the same Persom")
+        println("Tahiru and Iddrisu are not the same")
 
     val second_tahiru = Person("Tahiru","Fuseini",6.2,21,false)
     if (second_tahiru.equals(Tahiru))
-        println("It's the same Tahiru")
+        println("Tahiru and second_tahiru are the same")
     else
-        println("Nope ... not the same Tahiru")
+        println("Tahiru and second_tahiru are not the same")
 
     val copy_of_tahiru = Tahiru
     if (copy_of_tahiru.equals(Tahiru))
-        println("It's the same Tahiru")
+        println("Tahiru and copy_of_tahiru are the same")
     else
-        println("Nope ... not the same Tahiru")
+        println("Tahiru and copy_of_tahiru are not the same")
 }
