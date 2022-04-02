8f4a6163d84349b8681485cbfa63699b89fbb159
tifuseini <tifuseini@st.ug.edu.gh>
2022-04-02 20:45:39 +0000

    equals working

--- day025-Objects/src/main.kt
+++ day025-Objects/src/main.kt
@@ -22,4 +22,16 @@ fun main() {
         println("It's the same person")
     else
         println("Nope not the same Persom")
+
+    val second_tahiru = Person("Tahiru","Fuseini",6.2,21,false)
+    if (second_tahiru.equals(Tahiru))
+        println("It's the same Tahiru")
+    else
+        println("Nope ... not the same Tahiru")
+
+    val copy_of_tahiru = Tahiru
+    if (copy_of_tahiru.equals(Tahiru))
+        println("It's the same Tahiru")
+    else
+        println("Nope ... not the same Tahiru")
 }
